904762528208b21e9e35a45ffcc98c9e1a23d98f
VelanieNurShabrina <velanienurshabrina@gmail.com>
2025-11-06 09:04:35 +0700

    fifth try

--- src/HistoryChart.js
+++ src/HistoryChart.js
@@ -37,7 +37,8 @@ export default function HistoryChart({ apiBase, refreshInterval = 10 }) {
 
       // ✅ Map data dengan konversi waktu
       const mapped = sorted.map((d) => ({
-         time: new Date((d.timestamp - 25200) * 1000),
+         time: new Date((d.timestamp + 25200) * 1000),
+
     
         rssi: d.rssi,
         dbm: d.dbm,
