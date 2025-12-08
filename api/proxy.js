1e158b0495fd193f8b25f3cbf7b83f5384486d33
VelanieNurShabrina <velanienurshabrina@gmail.com>
2025-12-08 08:24:45 +0700

    fix cors

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,15 @@
 export default async function handler(req, res) {
-  // Ambil path setelah /api/
+  const NGROK = process.env.NGROK_URL;  // ⬅️ ambil dari environment
   const path = req.url.replace("/api/", "");
 
-  // URL backend via ngrok
-  const url = `https://heterophoric-franco-unplumbed.ngrok-free.dev/${path}`;
+  const url = `${NGROK}/${path}`;
 
   try {
-    const response = await fetch(url, {
-      method: req.method,
-    });
-
+    const response = await fetch(url, { method: req.method });
     const text = await response.text();
 
-    // Paksa selalu JSON supaya React tidak error
     res.setHeader("Content-Type", "application/json");
     return res.status(200).send(text);
-
   } catch (err) {
     return res.status(500).json({
       error: "Proxy error",
