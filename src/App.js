df479b539d0a8c94736437c864bf725f05615dbb
VelanieNurShabrina <velanienurshabrina@gmail.com>
2026-02-12 09:39:01 +0700

    fix ui 4

--- src/App.js
+++ src/App.js
@@ -108,24 +108,24 @@ function App() {
           <h2
             style={{
               margin: 0,
-              fontSize: 18,
+              fontSize: 30,
               fontWeight: 900,
               letterSpacing: 1.5,
               color: "#1e3a8a",
             }}
           >
-            📡 ISATPHONE NOC
+            📡 ISATPHONE
           </h2>
 
           <div
             style={{
-              fontSize: 11,
+              fontSize: 25,
               fontWeight: 600,
               color: "#64748b",
               marginTop: 2,
             }}
           >
-            Network Monitoring System
+            Monitoring System
           </div>
         </div>
 
